21a9e9763571ebf077f608a2d5f4b92cc1ceb704
Stephen <116318411+stephen-kollo@users.noreply.github.com>
2022-11-18 16:31:59 -0300

    sheet style function added

--- 04_printReport.gs.js
+++ 04_printReport.gs.js
@@ -1,176 +1,281 @@
-function createReport(sorted_data) {
-  console.log("------- Creating Report -------")
-  const report_data = combineData(sorted_data)
-  const report = {
-    report_data: report_data,
-    totals: countTotals(report_data),
-    regions: countByRegion(report_data),
-    apps: countByApp(report_data),
-  }
-  return report
-}
-
-function combineData(sorted_data) {
-  const combined_data = []
-
-  for(var j = 0; j < sorted_data.length; j++) {
-    const data = sorted_data[j].data
-    var combined = []
-
-    for(var i = 0; i < data.length; i++) {
-      const unitName = `${data[i].campaign_name.agent} / ${data[i].campaign_name.app} / ${data[i].campaign_name.region}`
-      if(combined.length > 0 && combined[combined.length-1].name == unitName) {
-        combined[combined.length-1].revenue += data[i].revenue
-        combined[combined.length-1].convertions += data[i].convertions
-        combined[combined.length-1].cost += data[i].cost
-        combined[combined.length-1].installs += data[i].installs
-      } else {
-        combined.push({
-          name: unitName,
-          cost: data[i].cost,
-          installs: data[i].installs,
-          revenue: data[i].revenue,
-          convertions: data[i].convertions,
-          tags: {
-            agent: data[i].campaign_name.agent,
-            app: data[i].campaign_name.app,
-            region: data[i].campaign_name.region
-          }
-        })
-      }
-    }
-
-    combined_data.push({
-      source_name: sorted_data[j].source_name,
-      data: combined,
-      errors: sorted_data[j].errors
-    })
-  }
-
-  console.log("..source and tracker data combined")
-  return combined_data
-}
-
-function countTotals(data) {
-   const totals = []
-   data.forEach(source => {
-     var total = {
-      source_name: source.source_name,
-      total_convertions: source.data.reduce((sum, unit) => {
-        return sum + unit.convertions;
-      }, 0),
-      total_revenue: source.data.reduce((sum, unit) => {
-        return sum + unit.revenue;
-      }, 0),
-      total_installs: source.data.reduce((sum, unit) => {
-        return sum + unit.installs;
-      }, 0),
-      total_cost: source.data.reduce((sum, unit) => {
-        return sum + unit.cost;
-      }, 0),
-      total_profit: 0,
-      total_roi: 0,
-    }
-    total.total_profit = total.total_revenue - total.total_cost
-    total.total_roi = (total.total_revenue - total.total_cost) / total.total_cost 
-
-    totals.push(total)
-   })
-
-   console.log("..totals counted")
-   return totals
-}
-
-function countByRegion(data) {
-  var regions_by_source = []
+function printReport(report) {
+  console.log("------- Printing Report -------")
+  const general_report_list = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("General report");
+  const campaign_report_list = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Campaign report");
+  const errors_list = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Error report");
   
-  data.forEach(source => {
-    var regions = []
-    const data = source.data
+  clearList(general_report_list)
+  clearList(campaign_report_list)
+  clearList(errors_list)
+  console.log("..report sheets cleaned!")
 
-    data.sort((a, b) => 
-      a.tags.region.localeCompare(b.tags.region) 
-    );
-    
-    for(var i = 0; i < data.length; i++) {
-      if(regions.length > 0 && regions[regions.length-1].region == data[i].tags.region) {
-        regions[regions.length-1].revenue += data[i].revenue
-        regions[regions.length-1].convertions += data[i].convertions
-        regions[regions.length-1].cost += data[i].cost
-        regions[regions.length-1].installs += data[i].installs
-      } else {
-        regions.push({
-          region: data[i].tags.region,
-          cost: data[i].cost,
-          installs: data[i].installs,
-          revenue: data[i].revenue,
-          convertions: data[i].convertions,
-          profit: 0,
-          roi: 0,
-        })
-      }
-    }
+  printTotals(report.totals, general_report_list)
+  printRegions(report.regions, general_report_list)
+  printApps(report.apps, general_report_list)
 
-    regions_by_source.push({
-      source: source.source_name,
-      regions: regions
+  printCombinedCampaingData(report.report_data, campaign_report_list)
+
+  printErrors(report.report_data, errors_list)
+  
+  console.log("================ REPORT PRINTED ================")
+}
+
+function printTotals(totlas, report_list) {
+  const column_names = [["Source", "Convertions", "Revenue", "Installs", "Cost", "Profit", "ROI"]]
+  report_list.getRange(1,1,1,7).setBorder(true,true,true,true,true,true).mergeAcross().setFontWeight("bold").setHorizontalAlignment("center").setBackground("#c3e8c3")
+  report_list.getRange(2,1,1,7).setValues(column_names).setFontWeight("bold").setBorder(true,true,true,true,true,true).setBackground("#dedede")
+  report_list.getRange(1,1).setValue("Totals")
+  
+  var data_array = []
+  totlas.forEach(source => {
+    data_array.push([
+      source.source_name.toString(), 
+      source.total_convertions.toString(),
+      source.total_revenue.toString(),
+      source.total_installs.toString(),
+      source.total_cost.toString(),
+      source.total_profit.toString(),
+      source.total_roi.toString()
+    ])
+  })
+  var last_row = report_list.getLastRow()
+  report_list.getRange(last_row + 1, 1, data_array.length, 7).setValues(data_array)
+  last_row = report_list.getLastRow()
+  styleTable(report_list, {
+    start_row: 1,
+    last_row: last_row,
+    start_column: 1,
+    last_column: 7
+  })
+  
+  console.log("..totals printed!")
+}
+
+function printRegions(region_data, report_list) {
+  region_data.forEach(source => {
+    const start_row = report_list.getLastRow() + 2
+    const column_names = [["Region", "Convertions", "Revenue", "Installs", "Cost", "Profit", "ROI"]]
+    report_list.getRange(start_row,1,1,7).setBorder(true,true,true,true,true,true).mergeAcross().setFontWeight("bold").setHorizontalAlignment("center").setBackground("#d1c3e8")
+    report_list.getRange(start_row + 1,1,1,7).setValues(column_names).setFontWeight("bold").setBorder(true,true,true,true,true,true).setBackground("#dedede")
+    report_list.getRange(start_row,1).setValue(`Regions: ${source.source}`)
+
+    var data_array = []
+    source.regions.forEach(region => {
+      data_array.push([
+        region.region.toString(), 
+        region.convertions.toString(),
+        region.revenue.toString(),
+        region.installs.toString(),
+        region.cost.toString(),
+        region.profit.toString(),
+        region.roi.toString()
+      ])
     })
-
-    regions.forEach(region => {
-      region.profit = region.revenue - region.cost
-      region.roi = (region.revenue - region.cost) / region.cost
+    var last_row = report_list.getLastRow()
+    report_list.getRange(last_row + 1, 1, data_array.length, 7).setValues(data_array)
+    last_row = report_list.getLastRow()
+    styleTable(report_list, {
+      start_row: start_row,
+      last_row: last_row,
+      start_column: 1,
+      last_column: 7
     })
-
-    regions.sort((a, b) => b.profit - a.profit);
   })
 
-  console.log("..regions counted")
-  return regions_by_source
+  console.log("..regions printed!")
 }
 
-function countByApp(data) {
-  var apps_by_source = []
-  
-  data.forEach(source => {
-    var apps = []
-    const data = source.data
-
-    data.sort((a, b) => 
-      a.tags.app.localeCompare(b.tags.app) 
-    );
-    
-    for(var i = 0; i < data.length; i++) {
-      if(apps.length > 0 && apps[apps.length-1].app == data[i].tags.app) {
-        apps[apps.length-1].revenue += data[i].revenue
-        apps[apps.length-1].convertions += data[i].convertions
-        apps[apps.length-1].cost += data[i].cost
-        apps[apps.length-1].installs += data[i].installs
-      } else {
-        apps.push({
-          app: data[i].tags.app,
-          cost: data[i].cost,
-          installs: data[i].installs,
-          revenue: data[i].revenue,
-          convertions: data[i].convertions,
-          profit: 0,
-          roi: 0,
-        })
-      }
-    }
-
-    apps_by_source.push({
-      source: source.source_name,
-      apps: apps
+function printApps(app_data, report_list) {
+  app_data.forEach(source => {
+    const start_row = report_list.getLastRow() + 2
+    const column_names = [["App", "Convertions", "Revenue", "Installs", "Cost", "Profit", "ROI"]]
+    report_list.getRange(start_row,1,1,7).setBorder(true,true,true,true,true,true).mergeAcross().setFontWeight("bold").setHorizontalAlignment("center").setBackground("#b1dce3")
+    report_list.getRange(start_row + 1,1,1,7).setValues(column_names).setFontWeight("bold").setBorder(true,true,true,true,true,true).setBackground("#dedede")
+    report_list.getRange(start_row,1).setValue(`Apps: ${source.source}`)
+   
+    var data_array = []
+    source.apps.forEach(app => {
+      data_array.push([
+        app.app.toString(), 
+        app.convertions.toString(),
+        app.revenue.toString(),
+        app.installs.toString(),
+        app.cost.toString(),
+        app.profit.toString(),
+        app.roi.toString()
+      ])
     })
-
-    apps.forEach(app => {
-      app.profit = app.revenue - app.cost
-      app.roi = (app.revenue - app.cost) / app.cost
+    var last_row = report_list.getLastRow()
+    report_list.getRange(last_row + 1, 1, data_array.length, 7).setValues(data_array)
+    last_row = report_list.getLastRow()
+    styleTable(report_list, {
+      start_row: start_row,
+      last_row: last_row,
+      start_column: 1,
+      last_column: 7
     })
-
-    apps.sort((a, b) => b.profit - a.profit);
   })
 
-  console.log("..apps counted")
-  return apps_by_source
+  console.log("..apps printed!")
+}
+
+function printCombinedCampaingData(report_data, report_list) {
+  report_data.forEach(source => {
+    let start_row
+    if(report_list.getLastRow() > 0) {
+      start_row = report_list.getLastRow() + 2
+    } else {
+      start_row = 1
+    }
+    const column_names = [["App", "Region", "Convertions", "Revenue", "Installs", "Cost", "Profit", "ROI"]]
+    report_list.getRange(start_row,1,1,8).setBorder(true,true,true,true,true,true).mergeAcross().setFontWeight("bold").setHorizontalAlignment("center").setBackground("#b1dce3")
+    report_list.getRange(start_row + 1,1,1,8).setValues(column_names).setFontWeight("bold").setBorder(true,true,true,true,true,true).setBackground("#dedede")
+    report_list.getRange(start_row,1).setValue(`Combined campaign data: ${source.source_name}`)
+   
+    var data_array = []
+    source.data.forEach(unit => {
+      const cost = Number(unit.cost)
+      const revenue = Number(unit.revenue)
+      data_array.push([
+        unit.tags.app.toString(), 
+        unit.tags.region.toString(), 
+        unit.convertions.toString(),
+        revenue.toString(),
+        unit.installs.toString(),
+        cost.toString(),
+        (revenue - cost).toString(),
+        ((revenue - cost) / cost).toString()
+      ])
+    })
+
+    var last_row = report_list.getLastRow()
+    report_list.getRange(last_row + 1, 1, data_array.length, 8).setValues(data_array)
+    last_row = report_list.getLastRow()
+    styleTable(report_list, {
+      start_row: start_row,
+      last_row: last_row,
+      start_column: 1,
+      last_column: 8
+    })
+  })
+
+  console.log("..combined campaign report printed!")
+}
+
+function printErrors(report_data, report_list) {
+  report_data.forEach(source => {
+    if(source.errors.length > 0) {
+
+      let start_row
+      if(report_list.getLastRow() > 0) {
+        start_row = report_list.getLastRow() + 2
+      } else {
+        start_row = 1
+      }
+      const column_names = [["Error name", "Located at", "Campaign name", "Convertions", "Revenue", "Installs", "Cost"]]
+      report_list.getRange(start_row,1,1,7).setBorder(true,true,true,true,true,true).mergeAcross().setFontWeight("bold").setHorizontalAlignment("center").setBackground("#b1dce3")
+      report_list.getRange(start_row + 1,1,1,7).setValues(column_names).setFontWeight("bold").setBorder(true,true,true,true,true,true).setBackground("#dedede")
+      report_list.getRange(start_row,1).setValue(`Error data: ${source.source_name}`)
+    
+      var data_array = []
+      source.errors.forEach(error => {
+        const location = Number(error.convertions) > 0 ? "tracker_data" : "source_data"
+        data_array.push([
+          error.campaign_name.error.error_name.toString(), 
+          location,
+          error.campaign_name.error.campaign_name.toString(),  
+          error.convertions.toString(),
+          error.revenue.toString(),
+          error.installs.toString(),
+          error.cost.toString(),
+        ])
+      })
+
+      var last_row = report_list.getLastRow()
+      report_list.getRange(last_row + 1, 1, data_array.length, 7).setValues(data_array)
+      last_row = report_list.getLastRow()
+      styleErrorTable(report_list, {
+        start_row: start_row,
+        last_row: last_row,
+        start_column: 1,
+        last_column: 7
+      })
+    }
+  })
+
+  console.log("..errors printed!")
+}
+
+function styleTable(list, range) {
+  list.getRange(
+    range.start_row + 2,
+    range.start_column,
+    range.last_row - 1 - range.start_row,
+    range.last_column
+  ).setBorder(true,true,true,true,true,true)
+
+  list.getRange(
+    range.start_row + 1,
+    range.start_column,
+    range.last_row - range.start_row,
+    range.last_column
+  ).setHorizontalAlignment("left")
+
+  list.getRange(
+    range.start_row + 2,
+    range.last_column,
+    range.last_row - range.start_row,
+    1
+  ).setNumberFormat("##.#%")
+
+  list.getRange(
+    range.start_row + 2,
+    range.last_column - 4,
+    range.last_row - range.start_row,
+    1
+  ).setNumberFormat("$#,##0.00;$(#,##0.00)")
+
+  list.getRange(
+    range.start_row + 2,
+    range.last_column - 2,
+    range.last_row - range.start_row,
+    2
+  ).setNumberFormat("$#,##0.00;$(#,##0.00)")
+}
+
+function styleErrorTable(list, range) {
+  list.getRange(
+    range.start_row + 2,
+    range.start_column,
+    range.last_row - 1 - range.start_row,
+    range.last_column
+  ).setBorder(true,true,true,true,true,true)
+
+  list.getRange(
+    range.start_row + 1,
+    range.start_column,
+    range.last_row - range.start_row,
+    range.last_column
+  ).setHorizontalAlignment("left")
+
+  list.getRange(
+    range.start_row + 2,
+    range.last_column,
+    range.last_row - range.start_row,
+    1
+  ).setNumberFormat("$#,##0.00;$(#,##0.00)")
+
+  list.getRange(
+    range.start_row + 2,
+    range.last_column - 2,
+    range.last_row - range.start_row,
+    1
+  ).setNumberFormat("$#,##0.00;$(#,##0.00)")
+}
+
+function clearList(list) {
+  const row = list.getLastRow() + 1
+  const column = list.getLastColumn() + 1
+  const range = list.getRange(1, 1, row, column)
+  range.deleteCells(SpreadsheetApp.Dimension.COLUMNS)
 }
