e43cfdabb60615c49d4e9e2ba58a337742db5617
Stephen <116318411+stephen-kollo@users.noreply.github.com>
2022-11-22 18:10:34 -0300

    ui connection

--- 00__main.gs.js
+++ 00__main.gs.js
@@ -1,7 +1,8 @@
-function main() {
-  const _env = createEnv()
+function __main(report_sheets, settings_sheets) {
+  const _env = createEnv(settings_sheets)
   const cleaned_data = cleanRawData(_env.raw_data_env)
   const data = processData(cleaned_data, _env.validation_data)
   const report = createReport(data)
-  printReport(report)  
+  printReport(report, report_sheets)  
+  return { errors: _env.errors }
 }
